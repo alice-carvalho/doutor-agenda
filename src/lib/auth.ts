d662ebf25dfe4475763191d94ae1f974ca697fe7
Alice <alicecarv.resende@gmail.com>
2025-05-29 10:49:32 -0300

    feat: add clinic data to user session and render it on sidebar

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,7 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { customSession } from "better-auth/plugins/custom-session";
+import { eq } from "drizzle-orm";
 
 import db from "@/db";
 import * as schema from "@/db/schema";
@@ -11,12 +13,35 @@ export const auth = betterAuth({
     schema,
   }),
   socialProviders: {
-   
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
+
+  plugins: [
+    customSession(async ({ user, session }) => {
+      const clinics = await db.query.usersToClinicsTable.findMany({
+        where: eq(schema.usersToClinicsTable.userId, user.id),
+        with: {
+          clinic: true,
+        },
+      });
+      // TODO: Ao adaptar para o usuário ter múltiplas clínicas, deve-se mudar esse código
+      const clinic = clinics?.[0];
+      return {
+        user: {
+          ...user,
+          clinic: {
+            id: clinic.clinicId,
+            name: clinic.clinic.name,
+          },
+        },
+        session,
+      };
+    }),
+  ],
+
   user: {
     modelName: "usersTable",
   },
